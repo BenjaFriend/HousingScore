f33b026e342270d1e9b46830959c002067e61bfc
Ben Hoffman <benjamin.hoffman.dev@gmail.com>
2022-04-19 17:12:11 -0400

    This will generate a table of all the attrbiutes on the page. Now i need to actaully score them somehow

--- source/client/scripts/clientHandler.js
+++ source/client/scripts/clientHandler.js
@@ -8,13 +8,28 @@
     }, 
     false);
 
-    // TODO: export this from the server or somethign in a common lib
+
+    class ScoreData
+    {
+        constructor(InAttributeName, InValue, InScore, InPossibleScore)
+        {
+          this.Name = InAttributeName;
+          this.Value = InValue || -1;
+          this.Score = InScore || -1;
+          this.PossibleScore = InPossibleScore || 10;
+        }
+    }
+
+    /**
+     * Container of some data for the house.
+     */
     class HouseData
     {
-        constructor(InAddress)
+        constructor(InAddress, AttributesArray)
         {
             this.Address = InAddress;
-            this.SqFootScore = 0;
+            // If AttributesArray isn't passed it'll initiate an empty array 
+            this.Attributes = AttributesArray || [];
         }
     };
   
@@ -26,8 +41,13 @@
         // Add a listener for scheudling
         document.querySelector("#setHouseLink").addEventListener("submit", setHouseLink);
 
-        var TestHouseData = new HouseData("Fake Address 123 Cary Strert");
-        CreateResultsTable(TestHouseData);
+        var TestHouseData = new HouseData("LOCAL Test Address 123 Cary NC");
+        TestHouseData.Attributes.push(new ScoreData("Price", 123456, 5));
+        TestHouseData.Attributes.push(new ScoreData("Square Foot", 1400, 4, 5));
+        TestHouseData.Attributes.push(new ScoreData("Number of Rooms", 3, 9));
+        TestHouseData.Attributes.push(new ScoreData("Bathrooms", 2.5, 10));
+        
+        CreateResultsDisplay(TestHouseData);
     }
 
     /** Get the value set by the user about what house they want to look at. This returns a URL from the text box */
@@ -42,6 +62,8 @@
      */
     function setHouseLink(e)
     {
+        ResetDisplayedHouseData();
+        
         // Stop the page from relaoding when you press the button
         e.preventDefault();
 
@@ -60,8 +82,16 @@
 
         xhr.onload = function()
         {
-            // Tell the user that their request has been recieved            
-            OnHouseDataRecieved(JSON.parse(xhr.responseText));
+            // Tell the user that their request has been recieved
+            if(xhr.status == 200)
+            {
+                OnHouseDataRecieved(JSON.parse(xhr.responseText));
+            }
+            else
+            {
+                AddErrorMessage("A bad URL was given, we couldn't query it! Change it and try again");
+                return;
+            }  
         };
 
         // Send the request to the server
@@ -79,40 +109,98 @@
     function OnHouseDataRecieved(responseJSON)
     {
         ResetDisplayedHouseData();
-        var responseDiv = GetResponseDiv();
 
-        var p = document.createElement("p");
-        var node = document.createTextNode(JSON.stringify(responseJSON));
-        p.appendChild(node);
-        responseDiv.appendChild(p);
-
-        // Make a table??
-
-        // Add the address as a header
-        var p = document.createElement("h2");
-        var node = document.createTextNode(responseJSON.Address);
-        p.appendChild(node);
-        responseDiv.appendChild(p);
-
-        CreateResultsTable(responseJSON);
-
-        return;
+        CreateResultsDisplay(responseJSON);
     }
 
     /** Creates a new table and appends it to the result div with info all about our house */
-    function CreateResultsTable(InHouseData)
+    function CreateResultsDisplay(InHouseData)
     {
         var responseDiv = GetResponseDiv();
+        var CurrentScore = 0;
+        var BestPossibleScore = 0;
 
-        // Add price
+        // Add the address as a header
+        {
+            var p = document.createElement("h2");
+            var node = document.createTextNode(InHouseData.Address);
+            p.appendChild(node);
+            responseDiv.appendChild(p);
+        }
 
-        // Square footage rank
+        {
+            var p = document.createElement("h3");
+            var node = document.createTextNode("House Score Chart");
+            p.appendChild(node);
+            responseDiv.appendChild(p);
+        }
 
-        // Number of rooms/bathrooms
+        // Create a table for the attributes
+        {
+            var table = document.createElement('table');
+            table.classList.add("ScoreTable");
+            table.style.border = '1px solid black';
+            
+            // Create table labels
+            {
+                var AtttributeLabel = document.createElement('th');
+                var AtttributeLabelText = document.createTextNode("Attribute");
+                AtttributeLabel.appendChild(AtttributeLabelText);
+                
+                var ValueLabel = document.createElement('th');
+                var ValueLabelText = document.createTextNode("Value");
+                ValueLabel.appendChild(ValueLabelText);
+    
+                var ScoreLabel = document.createElement('th');
+                var ScoreLabelText = document.createTextNode("Score");
+                ScoreLabel.appendChild(ScoreLabelText);
 
-        // Add commute time
+                var BestScoreLabel = document.createElement('th');
+                var BestScoreLabelText = document.createTextNode("Best Possible Score");
+                BestScoreLabel.appendChild(BestScoreLabelText);
 
-        // Add Lot size
+                table.appendChild(AtttributeLabel);
+                table.appendChild(ValueLabel);
+                table.appendChild(ScoreLabel);
+                table.appendChild(BestScoreLabel);
+            }
+            
+
+            // For each attribute in the house data
+            for(var i = 0; i < InHouseData.Attributes.length; i++) 
+            {
+                var AttributeObject = InHouseData.Attributes[i];
+
+                CurrentScore += AttributeObject.Score;
+                BestPossibleScore += AttributeObject.PossibleScore;
+
+                const Row = table.insertRow();
+
+                const NameCell = Row.insertCell();
+                NameCell.appendChild(document.createTextNode(AttributeObject.Name));
+                NameCell.setAttribute("id","testID");
+
+                const ValueCell = Row.insertCell();
+                ValueCell.appendChild(document.createTextNode(AttributeObject.Value));
+
+                const ScoreCell = Row.insertCell();
+                ScoreCell.appendChild(document.createTextNode(AttributeObject.Score));
+
+                const BestScoreCell = Row.insertCell();
+                BestScoreCell.appendChild(document.createTextNode(AttributeObject.PossibleScore));
+            }
+
+            responseDiv.appendChild(table);
+        }
+
+        // Display the total score
+        {
+            var p = document.createElement("h3");
+            var Percentage = (CurrentScore / BestPossibleScore) * 100;
+            var node = document.createTextNode("Total Score: " + CurrentScore + " / " + BestPossibleScore + " (" + Percentage +"%)");
+            p.appendChild(node);
+            responseDiv.appendChild(p);
+        }
     }
 
     /** Display an error message to the user in the response div */
--- source/server/eventHandler.js
+++ source/server/eventHandler.js
@@ -1,6 +1,8 @@
 "use strict";
 
 const axios = require('axios');
+const jsdom = require('jsdom');
+var JSDOM = jsdom.JSDOM;
 
 /** Export of a generic function that can parse the URL and give it to the correct function */
 module.exports = 
@@ -20,41 +22,128 @@ module.exports =
     }
 }
 
+class ScoreData
+{
+    constructor(InAttributeName, InValue, InScore, InPossibleScore)
+    {
+      this.Name = InAttributeName;
+      this.Value = InValue || -1;
+      this.Score = InScore || -1;
+      this.PossibleScore = InPossibleScore || 10;
+    }
+}
+
 /**
  * Container of some data for the house.
  */
 class HouseData
 {
-    constructor(InAddress)
+    constructor(InAddress, AttributesArray)
     {
       this.Address = InAddress;
-      this.Price = -1;
-      this.SqFoot = -1;
-      this.NumStories = -1;
-      this.MinutesToWork = -1;
-      this.HoaFee = -1;
-      this.NumRooms = -1;
-      this.NumBathrooms = -1.5;
-      this.bHasGarage = false;
-      this.LotSize = -1;
+      // If AttributesArray isn't passed it'll initiate an empty array 
+      this.Attributes = AttributesArray || [];
     }
 };
+
+function SanatizeString(InString) 
+{
+    return InString.replace(/\D/g,'');
+}
 
 /** Parse the given HTML source of the web page and create some house data for us to use */
 function ParseSourceHousePage(InPageText)
 {
-    var OutHouseData = new HouseData("Parsed house Data");
+    console.log("Parsing the web page...");
 
-    console.log(InPageText);
+    var OutHouseData = new HouseData("Parsed house Data Address goes here");
 
+    const dom = new JSDOM(InPageText);
+    const document = dom.window.document;
+
+    // Get the address
+    {
+        var AddressClass = "summary__Content-e4c4ok-3 btuvLw";
+        const AddressElem = document.getElementsByClassName(AddressClass)[0];
+
+        OutHouseData.Address = AddressElem.textContent;
+    }
     
+    // Get all the "normal" attributes about a house (the stuff in teh top right)
+    {
+        var Class = "summary__StyledSummaryDetailUnit-e4c4ok-4 kjpjnt";
+        const collection = document.getElementsByClassName(Class);
+
+        for(var i = 0; i < collection.length; i++)
+        {
+            var Label = collection[i].children[1].textContent;
+            var Value = collection[i].children[0].textContent;
+
+            if(Label.startsWith('$'))
+            {
+                continue;
+            }
+           
+            OutHouseData.Attributes.push(new ScoreData(Label, Value, 5));
+        }
+    }
+    
+
+    // Parse the data on the side bar like the county, lot size, and the year it was built
+    {
+        var LabelsWeCareAbout = 
+        [
+            "Year Built",
+            "Lot Size",
+            "Taxes",
+            "Compass Type",
+            "HOA Fees"
+        ];
+
+        var TableClass = "data-table__TableStyled-ibnf7p-0 bhHpMT";
+        const SideTable = document.getElementsByClassName(TableClass)[0];
+
+        for (var i = 0, row; row = SideTable.rows[i]; i++) 
+        {
+            var Label = row.children[0].textContent;
+            var Value = row.children[1].textContent;
+
+            if(!LabelsWeCareAbout.includes(Label) || Value == "-")
+            {
+                continue;
+            }
+
+            OutHouseData.Attributes.push(new ScoreData(Label, Value, 5));
+         }
+    }
+
+    // Iterate over the "Property information" panel at the bottom of the compass page
+    {
+        var DetailsClass = "property-information__FieldSection-sc-1il5vdr-7 kTiQQm textIntent-caption1";
+        const collection = document.getElementsByClassName(DetailsClass);
+
+        for(var i = 0; i < collection.length; i++)
+        {
+            var SplitArray = collection[i].textContent.split(":");
+            var Label = SplitArray[0];
+            var Value = SplitArray[1];
+
+            if(Label.startsWith('$'))
+            {
+                continue;
+            }
+           
+            OutHouseData.Attributes.push(new ScoreData(Label, Value, 5));
+        }
+    }
+
     return OutHouseData;
 }
 
 /** Called when the client has given the server a link to the house listing that they want scored */
 function setNewHouseLink(res, params)
 {
-    console.log("[setNewHouseLink] " + JSON.stringify(params));
+    //console.log("[setNewHouseLink] " + JSON.stringify(params));
 
     var houseUrl = params.houseUrl;
 
@@ -71,7 +160,7 @@ function setNewHouseLink(res, params)
     .get(houseUrl)
     .then(houseUrlRes => 
     {
-        var OutHouseData = ParseSourceHousePage(houseUrlRes);
+        var OutHouseData = ParseSourceHousePage(houseUrlRes.data);
 
         res.writeHead(200, { "Content-Type" : "text/html"} );
         res.write(JSON.stringify(OutHouseData));
